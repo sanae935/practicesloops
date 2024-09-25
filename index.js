c62721eb4973a7670235c545b3fc34adffc3d8e2
sanae935 <s.elatmani005@gmail.com>
2024-09-24 23:27:36 -0400

    index.js file

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-//
+//Part 2: Practical Exercises
+//Multiplication Table with For Loop:
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} x ${j} = ${i * j}`);
+    }
+    console.log('------------');
+}
